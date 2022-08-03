bf668b0a2b13e564757688a0c691a765c038bbe8
박준수 <qwnstn@naver.com>
2022-08-03 23:43:11 +0900

    220803

---
+++ 2208/BOJ/11653_timeout_2.py
@@ -0,0 +1,35 @@
+# 시간 초과
+n = int(input())
+
+num_list = [1]              # 1 추가
+
+for i in range(2, int(n**0.5) +1):       # 2의 배수 추가, 2 제거
+    num_list.append(i*2)
+
+for i in range(1, int(n**0.5) +1):       # 3부턴 홀수만 시행
+    p= i*2 + 1                           # 있는건 시행 X
+    if p in num_list:
+        pass
+    else:
+        for j in range(2, n//p +1): 
+            num_list.append(j * p)
+
+num_list = set(num_list)                # 중복 제거(set), 소수를 제외한 숫자들
+
+p_list = list(range(1, int(n**0.5) + 1))
+for i in num_list:
+    if i in p_list:
+        p_list.remove(i)                # 소수 리스트 제작
+
+
+while n not in p_list:
+    for i in p_list:
+        if n % i == 0:
+            n = n // i
+            print(i)
+            break
+        else:
+            if i == p_list[-1]:
+                p_list.append(n)
+                break
+print(n)
